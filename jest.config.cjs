b453537a088d025ecb442469c2601a3bc3f882d4
CaonVini <83681064+CaonVini@users.noreply.github.com>
2026-03-19 20:05:02 -0300

    chore: setup inicial do projeto

---
+++ jest.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.vue$': '@vue/vue3-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+};
